16414f9e490e35dbb1e201d1e8beef28751c3747
Daniel Romero <danieljromero305@gmail.com>
2024-04-04 14:11:58 -0400

    Added header to js file

---
+++ res/base.js
@@ -0,0 +1,12 @@
+// need to load header across every page, easier to load this function than copy and paste code every time
+// plus when one change is made here the change is made across every page
+window.onload = function () {
+    document.getElementById("header").innerHTML = "<div id=\"menu\" class=\"flex-container\">\n" +
+        "        <div><a href=\"index.html\">Observatory</a></div>\n" +
+        "        <div><a href=\"contact.html\">Contact Us</a></div>\n" +
+        "        <div><a href=\"appointments.html\">Appointments</a></div>\n" +
+        "        <div><a href=\"about.html\">About Us</a></div>\n" +
+        "        <div><a href=\"calendar.html\">Calendar</a></div>\n" +
+        "        <div><a href=\"research.html\">Research</a></div>\n" +
+        "    </div>";
+}
