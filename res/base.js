9ffa6e6d69346def3f7cc6398db52bc7b57de031
santino0413 <santino.navaroli@gmail.com>
2024-04-12 11:35:27 -0400

    Changed IFrame video to a Picture of the Ring Nebula.

--- res/base.js
+++ res/base.js
@@ -15,21 +15,3 @@ function loadHeader() {
             "<div><a href='research.html'>Research</a></div>\n" +
         "</div>";
 }
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
-let header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
-let sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
