70f5b81a05055e3622a7439ade88ae42fecc5daf
Daniel Romero <danieljromero305@gmail.com>
2024-04-16 11:43:26 -0400

    Webstorm reformat

--- res/base.js
+++ res/base.js
@@ -7,19 +7,21 @@ window.onload = function () {
 
 function loadHeader() {
     document.getElementById("header").innerHTML =
-        "<span id='menu'><div><a href='index.html'><img alt='University of North Georgia Logo' id='headericon' src='res/UNG-Icon.jpg'> North Georgia Astronomical Observatory</a></div>\n" +
+        "<span id='menu'>" +
+        "<div><a href='index.html'><img alt='University of North Georgia Logo' id='headericon' src='res/UNG-Icon.jpg'> North Georgia Astronomical Observatory</a></div>\n" +
         "<div class='flex-container'>" +
-            "<div><a href='contact.html'>Contact Us</a></div>\n" +
-            "<div><a href='appointments.html'>Appointments</a></div>\n" +
-            "<div><a href='about.html'>About Us</a></div>\n" +
-            "<div><a href='calendar.html'>Calendar</a></div>\n" +
-            "<div><a href='research.html'>Research</a></div>\n" +
-        "</div></span>";
+        "<div><a href='contact.html'>Contact Us</a></div>\n" +
+        "<div><a href='appointments.html'>Appointments</a></div>\n" +
+        "<div><a href='about.html'>About Us</a></div>\n" +
+        "<div><a href='calendar.html'>Calendar</a></div>\n" +
+        "<div><a href='research.html'>Research</a></div>\n" +
+        "</div>" +
+        "</span>";
 }
 
-const headers = ["crab_chandra.jpg", "crab-nebula-mosaic.jpg", "hubble deep field.webp", "MeerKAT on SDSS.png", "sombrero-galaxy.jpg", "Space1.jfif"];
+function chooseImage() {
+    const headers = ["crab_chandra.jpg", "crab-nebula-mosaic.jpg", "hubble deep field.webp", "MeerKAT on SDSS.png", "sombrero-galaxy.jpg", "Space1.jfif"];
 
-function chooseImage(){
     let random = Math.floor(Math.random() * headers.length);
     document.getElementById("header_image").setAttribute("src", "res/images/" + headers[random]);
 }
